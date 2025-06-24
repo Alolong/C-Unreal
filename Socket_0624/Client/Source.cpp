2001e671ef40c7b25b5513fff28144a88fbf4106
Alolong <ddten123@gmail.com>
2025-06-24 17:47:14 +0900

    server

---
+++ Socket_0624/Client/Source.cpp
@@ -0,0 +1,43 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream>
+#include <WinSock2.h>
+#include "Packet.h"
+
+using namespace std;
+
+#pragma comment(lib,"ws2_32")
+
+int main()
+{
+	WSAData wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	
+	SOCKET ServerSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	SOCKADDR_IN ServerSocketAddr;
+	memset(&ServerSocketAddr, 0, sizeof(ServerSocketAddr));
+	ServerSocketAddr.sin_family = PF_INET;
+	ServerSocketAddr.sin_addr.s_addr = inet_addr("127.0.01");
+	ServerSocketAddr.sin_port = htons(32000);
+
+
+	connect(ServerSocket, (SOCKADDR*)&ServerSocketAddr, sizeof(ServerSocketAddr));
+
+	
+
+		char Buffer[1024] = { 0, };
+		recv(ServerSocket, Buffer, 1024, 0);
+		cout << "Server Send:" << Buffer << endl;
+
+		send(ServerSocket, Buffer, strlen(Buffer), 0);
+
+
+
+		closesocket(ServerSocket);
+	
+
+	WSACleanup();
+
+	return  0;
+
+}
---
+++ Socket_0624/Common/Packet.h
@@ -0,0 +1,22 @@
+#pragma once
+
+enum class EEventCode
+{
+	C2S_Plus = 1,
+	C2S_Minus = 2,
+	C2S_Multyply = 3,
+	C2S_Divide = 4,
+	C2S_Remain = 5,
+	S2C_Result = 6,
+};
+//구조체 생성하면 자동화 가능 
+struct Numbers
+{
+	short FirstNumber;
+	short SecondNumbers;
+};
+
+struct Result
+{
+	int Number;
+};
